bf22d3ca57832c114f5d57ce2b07fe6a33c68a5e
Surinda Sarakum <surindasarakum18@gmail.com>
2021-06-13 15:43:56 +0700

    Add project files.

---
+++ final1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace final1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = new int[3, 3];
+            Console.WriteLine("Input the arrays:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    Console.Write("{0} : ", i);
+                    numbers[i, x] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    Console.Write(numbers[i, x] + " ");
+                }
+                Console.WriteLine("");
+            }
+           Console.ReadLine();
+
+        }
+    }
+}
